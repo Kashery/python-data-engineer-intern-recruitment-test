901a8a0efd32f5eab09d78219263a4e3d7baf951
Kashery <59961055+Kashery@users.noreply.github.com>
2020-11-17 15:49:54 +0100

    chart scirpt update
    "by genre" chart looks better on x-axis-label thanks to some tinkering with fontsize and rotation of text

--- chart.py
+++ chart.py
@@ -5,5 +5,6 @@ a= pd.read_csv(os.path.join(os.path.dirname(__file__), "movie_rating_by_year.tsv
 a.plot.scatter(x='year', y='averageRating')
 plt.savefig(os.path.join(os.path.dirname(__file__), 'movie_rating_by_year.png'))
 a= pd.read_csv(os.path.join(os.path.dirname(__file__), "movie_rating_by_genres.tsv"),sep='\t',low_memory=False,memory_map=True)
-a.plot.scatter(x='genre', y='averageRating').tick_params(axis='x', rotation=-70,direction= 'in')
+a.plot.scatter(x='genre', y='averageRating', s=20).tick_params(axis='x', rotation=-37,direction= 'in',labelsize=7)
+
 plt.savefig(os.path.join(os.path.dirname(__file__), 'movie_rating_by_genre.png'))
